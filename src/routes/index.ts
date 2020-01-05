f28c2d25a0ddb843649a9ad4355f6ee8cda1ccdb
Ihor Klymenok <ihor.klymenok@outlook.com>
2020-01-05 18:12:28 +0000

    feat: add authentication middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,13 @@ import { getAllUsers } from '../db/models/users';
 import { signInUser, signUpUser } from "../services/users";
 import * as jwt from 'jsonwebtoken';
 import { config } from '../shared/config';
+import { authorize } from '../middlewares/authentication';
 
 export const router = Router();
 
 router.post('/sign-in', (req, res) => {
   signInUser(req.body)
-    .then(user => jwt.sign(user.password, config('JWT_SECRET')))
+    .then(user => jwt.sign(user, config('JWT_SECRET')))
     .then(token => res.json({ token }))
     .catch(err => res.json({ error: err.message }))
 })
@@ -19,10 +20,8 @@ router.post('/sign-up', (req, res) => {
     .catch(err => res.json({ error: err.msg }))
 })
 
-router.get('/users', (req, res) => {
+router.get('/users', authorize, (req, res) => {
   getAllUsers()
-    .then(users => {
-      res.json(users)
-    })
+    .then(users => res.json(users))
     .catch(err => res.json({ error: err.msg }));
 })
